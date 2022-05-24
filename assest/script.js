10edc4ee4973f368b96f8766f04c92d8019eb237
SahilMamedov <sahilim@code.edu.az>
2022-05-24 14:43:26 +0400

    Feat: custom task btn onclick

--- assest/script.js
+++ assest/script.js
@@ -3,4 +3,18 @@ let list=document.getElementById("list")
 let input=document.getElementById("input")
 let btnRemove=document.getElementById("btn-remove")
 
+btn.onclick=function(){
+    let checkarr=[];
+let inputValue=input.value.trim();
 
+if(inputValue!=""){
+    let li=document.createElement("li");
+li.innerText=inputValue;
+
+li.classList.add("list-group-item","liList")
+list.append(li);
+input.value="";
+let inputcheck=document.createElement("input");
+inputcheck.type="checkbox";
+inputcheck.classList="inputcheck"
+li.append(inputcheck)
