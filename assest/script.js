682c6cefa0c955f827a682b64ad378654600af6a
SahilMamedov <sahilim@code.edu.az>
2022-05-24 14:42:19 +0400

    feat: custom task get elemet by id

--- assest/script.js
+++ assest/script.js
@@ -1,3 +1,6 @@
-
+let btn = document.getElementById("btn")
+let list=document.getElementById("list")
+let input=document.getElementById("input")
+let btnRemove=document.getElementById("btn-remove")
 
 
